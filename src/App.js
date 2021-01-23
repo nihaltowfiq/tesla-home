19770a7ed362c5bd5361faf5e2580086d7b3bbc9
nihaltowfiq <nihaltowfiq@gmail.com>
2021-01-23 20:49:07 +0600

    expandBtn issue solved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.css";
-import Header from "./components/Header";
-
 import Accessories from "./assets/Desktop-Accessories.jpg";
 import Model3 from "./assets/Desktop-Model3.jpeg";
 import ModelS from "./assets/Desktop-ModelS.jpeg";
@@ -8,6 +6,7 @@ import ModelX from "./assets/Desktop-ModelX.jpeg";
 import ModelY from "./assets/Desktop-ModelY.jpeg";
 import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
 import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
+import Header from "./components/Header";
 import Item from "./components/Item";
 
 function App() {
